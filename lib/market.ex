d049c828912926f5271443e713495c31873d017a
Sergio Arbeo <serabe@gmail.com>
2025-03-06 02:03:17 +0100

    Initial commit with some basic extra configuration.

---
+++ lib/market.ex
@@ -0,0 +1,18 @@
+defmodule Market do
+  @moduledoc """
+  Documentation for `Market`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Market.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
